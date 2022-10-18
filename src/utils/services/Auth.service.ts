79d97276591862ec7ee1dfefd44ee5e1046b7c63
taesung1993 <cheonyulin@gmail.com>
2022-10-18 15:27:47 +0900

    restore limit time

--- src/utils/services/Auth.service.ts
+++ src/utils/services/Auth.service.ts
@@ -4,7 +4,7 @@ import LocalStorageService from "./LocalStorage.service";
 
 class AuthService {
   accessToken: string | null = null;
-  limitTime = 5000;
+  limitTime = 60 * 60 * 1000;
 
   login(body: { email: string; password: string }) {
     return axios.post<IAccessToken>(
