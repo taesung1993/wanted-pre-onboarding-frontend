f3e60521eff8d451ee799926b397fbccc21880c1
taesung1993 <cheonyulin@gmail.com>
2022-10-18 18:32:27 +0900

    add regex constants

---
+++ src/utils/constants/regex.ts
@@ -0,0 +1,3 @@
+export const emailRegex =
+  /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+export const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])[\da-zA-Z]{8,}$/;
