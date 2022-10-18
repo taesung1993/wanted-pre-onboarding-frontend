27340692a6acf227c1ba89208bcb0d44f87dbc69
taesung1993 <cheonyulin@gmail.com>
2022-10-18 20:19:57 +0900

    add and appy global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
+import GlobalStyle from "./components/GlobalStyle";
 import Routers from "./components/Routers";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <RouterProvider router={Routers} />
     </div>
   );
